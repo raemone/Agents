ea3a78a0a5f226dd60c4d952611d4b39a1ae96e4
Tracy Boehrer <trboehre@microsoft.com>
2025-04-02 11:08:57 -0500

    DotNet Echo:  Add OpenTelemetry sample (POC)

--- samples/basic/echo-bot/dotnet/Program.cs
+++ samples/basic/echo-bot/dotnet/Program.cs
@@ -1,51 +1,93 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using EchoBot;
-using Microsoft.Agents.BotBuilder.App;
-using Microsoft.Agents.BotBuilder.State;
+using Microsoft.Agents.Builder;
+using Microsoft.Agents.Builder.App;
 using Microsoft.Agents.Hosting.AspNetCore;
 using Microsoft.Agents.Samples;
-using Microsoft.Agents.Storage;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Threading;
+using Microsoft.Agents.Core.Models;
+using Microsoft.Agents.Storage;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHttpClient();
+
 builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
-// Add AspNet token validation
-builder.Services.AddBotAspNetAuthentication(builder.Configuration);
+// OpenTelemetry
+builder.AddServiceDefaults();
+builder.Services.AddProblemDetails();
 
-// Add ApplicationOptions
-builder.Services.AddTransient(sp =>
+// Register IStorage for the Agent.  For development, MemoryStorage is suitable.
+// For production Agents, persisted storage should be used so
+// that state survives Agent restarts, and operate correctly
+// in a cluster of Agent instances.
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+// Add AgentApplicationOptions from config.
+builder.AddAgentApplicationOptions();
+
+// Add the Agent
+builder.AddAgent(sp =>
 {
-    return new AgentApplicationOptions()
+    var app = new AgentApplication(sp.GetRequiredService<AgentApplicationOptions>());
+
+    app.OnConversationUpdate(ConversationUpdateEvents.MembersAdded, async (turnContext, turnState, cancellationToken) =>
     {
-        StartTypingTimer = false,
-        TurnStateFactory = () => new TurnState(sp.GetService<IStorage>())
-    };
+        foreach (ChannelAccount member in turnContext.Activity.MembersAdded)
+        {
+            if (member.Id != turnContext.Activity.Recipient.Id)
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("Hello and Welcome!"), cancellationToken);
+            }
+        }
+    });
+
+    app.OnActivity(ActivityTypes.Message, async(turnContext, turnState, cancellationToken) =>
+    {
+        await turnContext.SendActivityAsync($"You said: {turnContext.Activity.Text}", cancellationToken: cancellationToken);
+    });
+
+    return app;
 });
 
-// Add the bot (which is transient)
-builder.AddBot<MyBot>();
+// Configure the HTTP request pipeline.
+
+// Add AspNet token validation
+builder.Services.AddAgentAspNetAuthentication(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+var route = app.MapPost(
+    "/api/messages",
+    async (HttpRequest request, HttpResponse response, IAgentHttpAdapter adapter, IAgent agent, CancellationToken cancellationToken) =>
+    {
+        await adapter.ProcessAsync(request, response, agent, cancellationToken);
+    })
+    .RequireAuthorization(new AuthorizeAttribute("AllowedCallers"));
 
 
-WebApplication app = builder.Build();
+// Setup port and listening address.
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapGet("/", () => "Microsoft Agents SDK Sample");
-    app.UseDeveloperExceptionPage();
-    app.MapControllers().AllowAnonymous();
+    route.AllowAnonymous();
+    var port = args.Length > 0 ? args[0] : "3978";
+    app.Urls.Add($"http://localhost:{port}");
 }
-else
-{
-    app.MapControllers();
-}
-app.Run();
 
+// Start listening. 
+await app.RunAsync();
