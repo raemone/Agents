152c47b6ce4137187e423242b8d03345076f347f
Rodrigo Brandão <robrandao@microsoft.com>
2025-08-06 10:28:27 -0700

    Added basic logging config and updated README

--- samples/basic/azureai-streaming-poem-agent/python/src/main.py
+++ samples/basic/azureai-streaming-poem-agent/python/src/main.py
@@ -1,6 +1,13 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# enable logging for Microsoft Agents library
+# for more information, see README.md for empty-agent sample
+import logging
+ms_agents_logger = logging.getLogger("microsoft.agents")
+ms_agents_logger.addHandler(logging.StreamHandler())
+ms_agents_logger.setLevel(logging.INFO)
+
 from .agent import AGENT_APP, CONNECTION_MANAGER
 from .start_server import start_server
 
--- samples/basic/copilotstudio-client/python/src/main.py
+++ samples/basic/copilotstudio-client/python/src/main.py
@@ -1,9 +1,15 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License.
 
+# enable logging for Microsoft Agents library
+# for more information, see README.md for empty-agent sample
+import logging
+ms_agents_logger = logging.getLogger("microsoft.agents")
+ms_agents_logger.addHandler(logging.StreamHandler())
+ms_agents_logger.setLevel(logging.INFO)
+
 import sys
 from os import environ
-import logging
 import asyncio
 import webbrowser
 
